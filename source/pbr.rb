bc40677e6da65d70622a8d2adefaface5af0c495
Samuel Tallet-Sabathé <samuel.tallet@gmail.com>
2018-06-21 20:47:43 +0200

    Update version number.

--- source/pbr.rb
+++ source/pbr.rb
@@ -26,7 +26,7 @@ require 'extensions'
 # PBR plugin namespace.
 module PBR
 
-  VERSION = '1.3.5'.freeze
+  VERSION = '1.3.6'.freeze
 
   # Load translation if it's available for current locale.
   TRANSLATE = LanguageHandler.new('pbr.strings')
